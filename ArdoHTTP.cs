43114b33543b49c831c77cd7533e378f1fa797e1
joaobarata <14177189+joaobarata@users.noreply.github.com>
2026-08-10 18:35:13 +0100

    Upgrade to .Net10

--- ArdoHTTP.cs
+++ ArdoHTTP.cs
@@ -2,21 +2,36 @@
 using OutSystems.ExternalLibraries.SDK;
 using System.Collections.Generic;
 using System.Reflection.PortableExecutable;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using Tavis.UriTemplates;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace Ardo.ArdoHTTP
 {
     public class ArdoHTTP : IArdoHTTP
     {
 
-        private static readonly HttpClient client = new HttpClient();
+        private readonly ILogger? _logger;
+
+        public ArdoHTTP(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public ArdoHTTP()
+        {
+            _logger = null;
+        }
+        private static readonly HttpClientHandler handler = new HttpClientHandler();
         public string GetHeader(string Name, IEnumerable<HttpHeader> Headers)
         {
 
             foreach (var header in Headers)
             {
-                if(header.Name == Name)
+                if (header.Name == Name)
                 {
                     return header.Value;
                 }
@@ -29,6 +44,7 @@ namespace Ardo.ArdoHTTP
         /// </summary>
         public void HTTPDelete(string URL, out string Status, out int Status_Code, out IEnumerable<HttpHeader> Response_Headers, out string Response, IEnumerable<HttpHeader>? Headers, string? Username, string? Password)
         {
+            using var activity = Activity.Current?.Source.StartActivity("ArdoHTTP.HTTPDelete");
             processRequest(HttpMethod.Delete, URL, Headers, Username, Password, string.Empty, string.Empty, out Status, out Status_Code, out Response_Headers, out Response);
         }
         /// <summary>
@@ -36,6 +52,7 @@ namespace Ardo.ArdoHTTP
         /// </summary>
         public void HTTPGet(string URL, out string Status, out int Status_Code, out IEnumerable<HttpHeader> Response_Headers, out string Response, IEnumerable<HttpHeader>? Headers, string? Username, string? Password)
         {
+            using var activity = Activity.Current?.Source.StartActivity("ArdoHTTP.HTTPGet");
             processRequest(HttpMethod.Get, URL, Headers, Username, Password, string.Empty, string.Empty, out Status, out Status_Code, out Response_Headers, out Response);
         }
         /// <summary>
@@ -43,13 +60,15 @@ namespace Ardo.ArdoHTTP
         /// </summary>
         public void HTTPHead(string URL, out string Status, out int Status_Code, out IEnumerable<HttpHeader> Response_Headers, IEnumerable<HttpHeader>? Headers, string? Username, string? Password)
         {
-            processRequest(HttpMethod.Head, URL, Headers, Username?? string.Empty, Password ?? string.Empty, string.Empty, string.Empty, out Status, out Status_Code, out Response_Headers, out string _);
+            using var activity = Activity.Current?.Source.StartActivity("ArdoHTTP.HTTPHead");
+            processRequest(HttpMethod.Head, URL, Headers, Username ?? string.Empty, Password ?? string.Empty, string.Empty, string.Empty, out Status, out Status_Code, out Response_Headers, out string _);
         }
         /// <summary>
         /// Perform an HTTP PATCH request.
         /// </summary>
         public void HTTPPatch(string URL, string Data, string Content_Type, out string Status, out int Status_Code, out IEnumerable<HttpHeader> Response_Headers, out string Response, IEnumerable<HttpHeader>? Headers, string? Username, string? Password)
         {
+            using var activity = Activity.Current?.Source.StartActivity("ArdoHTTP.HTTPPatch");
             processRequest(HttpMethod.Patch, URL, Headers, Username, Password, Data, Content_Type, out Status, out Status_Code, out Response_Headers, out Response);
         }
         /// <summary>
@@ -57,18 +76,21 @@ namespace Ardo.ArdoHTTP
         /// </summary>
         public void HTTPPost(string URL, string Data, string Content_Type, out string Status, out int Status_Code, out IEnumerable<HttpHeader> Response_Headers, out string Response, IEnumerable<HttpHeader>? Headers, string? Username, string? Password)
         {
-            processRequest(HttpMethod.Post, URL, Headers, Username, Password, Data, Content_Type, out Status, out Status_Code, out Response_Headers, out Response);
+           using var activity = Activity.Current?.Source.StartActivity("ArdoHTTP.HTTPPost");
+           processRequest(HttpMethod.Post, URL, Headers, Username, Password, Data, Content_Type, out Status, out Status_Code, out Response_Headers, out Response);
         }
         /// <summary>
         /// Perform an HTTP PUT request.
         /// </summary>
         public void HTTPPut(string URL, string Data, string Content_Type, out string Status, out int Status_Code, out IEnumerable<HttpHeader> Response_Headers, out string Response, IEnumerable<HttpHeader>? Headers, string? Username, string? Password)
         {
+            using var activity = Activity.Current?.Source.StartActivity("ArdoHTTP.HTTPPut");
             processRequest(HttpMethod.Put, URL, Headers, Username, Password, Data, Content_Type, out Status, out Status_Code, out Response_Headers, out Response);
         }
 
         public string URLTemplate(string Template, IEnumerable<TemplateVar> Variables)
         {
+            using var activity = Activity.Current?.Source.StartActivity("ArdoHTTP.URLTemplate");
             UriTemplate template = new(Template);
 
             foreach (TemplateVar myVar in Variables)
@@ -88,16 +110,16 @@ namespace Ardo.ArdoHTTP
 
             request.Method = method;
 
-            if (request.Method == HttpMethod.Patch || request.Method == HttpMethod.Post || request.Method == HttpMethod.Put ) {
+            if (request.Method == HttpMethod.Patch || request.Method == HttpMethod.Post || request.Method == HttpMethod.Put)
+            {
 
-                request.Content = new StringContent(data?? string.Empty, Encoding.UTF8, contentType ?? "text/plain");
+                request.Content = new StringContent(data ?? string.Empty, Encoding.UTF8, contentType ?? "text/plain");
             }
 
-            
 
-            bool removeAuthHeader = true;
 
-            if (Headers != null) {
+            if (Headers != null)
+            {
                 foreach (var header in Headers)
                 {
                     switch (header.Name)
@@ -115,42 +137,35 @@ namespace Ardo.ArdoHTTP
                         case "Content-Type":
                         case "Connection":
                         case "Transfer-Encoding":
-                        case "Host": // auto-set
                         case "Date": // auto-set
                         case "Content-Length":
                         case "Timeout":
                         case "NoPreAuth":
                         case "NoAutoCompression":
                             break;
-                        case "Authorization":
-                            removeAuthHeader = false;
-                            break;
                         default:
-                            request.Headers.Add(header.Name,header.Value);
+                            request.Headers.Add(header.Name, header.Value);
                             break;
                     }
                 }
             }
 
-            if (removeAuthHeader)
+            if (Username != null && Username != string.Empty && Password != null && Password != string.Empty)
             {
-                client.DefaultRequestHeaders.Remove("Authorization");
-                request.Headers.Remove("Authorization");
+                if (request.Headers.Contains("Authorization"))
+                {
+                    request.Headers.Remove("Authorization");
+                }
+                request.Headers.Add("Authorization", "Basic " + Encode64(Username + ":" + Password));
             }
-
-            if (Username != null && Username != string.Empty && Password != null && Password !=string.Empty) {
-                request.Headers.Add("Authorization", "Basic " + Encode64(Username+":"+ Password));
-            }
-            
 
             try
             {
+                var client = new HttpClient(handler);
+
                 using HttpResponseMessage response = client.Send(request);
 
-               
-
                 using var reader = new StreamReader(response.Content.ReadAsStream());
-
 
                 ResponseContent = reader.ReadToEnd();
                 Status_Code = (int)response.StatusCode;
@@ -162,11 +177,15 @@ namespace Ardo.ArdoHTTP
                 foreach (var hh in h)
                 {
                     Response_Headers = Response_Headers.Append(new HttpHeader(hh.Key, hh.Value.First()));
-                } 
+                }
             }
             catch (HttpRequestException e)
             {
-                throw e;
+                if(_logger != null)  _logger.LogInformation($"{e.StatusCode} - {e.StatusCode.ToString()} {e.Message}");
+                Status_Code = e.StatusCode == null ? 500 : (int) e.StatusCode ;
+                ResponseContent = e.Message;
+                Status = e.Message;
+                Response_Headers = Enumerable.Empty<HttpHeader>();
 
             }
 
